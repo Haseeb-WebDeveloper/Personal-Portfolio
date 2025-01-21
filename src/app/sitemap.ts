90c934d4064d2c40bb7fffee8346bf97485735c3
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-01-21 14:02:33 +0500

    again sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -37,12 +37,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'daily',
       priority: 0.7,
     },
-    {
-      url: `https://jsonformatteronline.site/`,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    },
     ...blogUrls,
   ]
 }
